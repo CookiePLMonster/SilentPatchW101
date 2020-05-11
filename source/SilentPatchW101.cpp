8fb9df1a806a96b752c855cc7edd984b1b257f9f
Silent <zdanio95@gmail.com>
2020-05-11 21:18:17 +0200

    Don't sleep in a frame limiter

--- source/SilentPatchW101.cpp
+++ source/SilentPatchW101.cpp
@@ -4,5 +4,14 @@
 
 void OnInitializeHook()
 {
+	using namespace Memory::VP;
+	using namespace hook;
 
+	// Don't sleep in a frame limiter
+	// This affects Wonderful 101, Metal Gear Rising and who knows how many more games from Platinum Games
+	auto sleep = pattern( "85 D2 7E 03" ).count(1);
+	if ( sleep.size() == 1 )
+	{
+		Patch<uint8_t>( sleep.get_first<void>( 2 ), 0xEB ); // jle -> jmp
+	}
 }
